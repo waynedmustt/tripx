f285371379f8f8c5b1ea2cd8ade7764545909d4a
waynedmustt <muhammaddimas04@gmail.com>
2022-05-14 12:21:42 +0800

    Initial commit.

---
+++ src/tripx_assets/src/index.js
@@ -0,0 +1,19 @@
+import { tripx } from "../../declarations/tripx";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await tripx.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
