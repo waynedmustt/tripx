c6126515547165c1165fc0238aa6775e966a1e95
waynedmustt <muhammaddimas04@gmail.com>
2022-06-17 22:04:01 +0800

    finishing login stoic wallet

--- src/tripx_assets/src/pages/login/index.jsx
+++ src/tripx_assets/src/pages/login/index.jsx
@@ -1,8 +1,41 @@
 import * as React from "react";
+import {
+    Button
+  } from 'react-bootstrap';
+import { StoicIdentity } from "ic-stoic-identity";
 
 const Login = () => {
+    const [principal, setPrincipal] = React.useState('')
+
+    const loginStoic = async () => {
+        StoicIdentity.load().then(async identity => {
+            if (identity !== false) {
+                //ID is a already connected wallet!
+            } else {
+                //No existing connection, lets make one!
+                identity = await StoicIdentity.connect();
+            }
+            
+            //Lets display the connected principal!
+            setPrincipal(identity.getPrincipal().toText());
+        })
+    }
+
+    const logout = () => {
+        //Disconnect after
+        StoicIdentity.disconnect();
+        setPrincipal('')
+    }
+
     return (
-        <div>Login works!</div>
+        <React.Fragment>
+            <Button variant="primary" type="submit" onClick={loginStoic}>
+                {principal ? principal : 'Login with Stoic Wallet'}
+            </Button>
+            {principal && <Button variant="primary" type="submit" onClick={logout}>
+                {'Logout'}
+            </Button>}
+        </React.Fragment>
     );
 }
 
